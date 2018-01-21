11188de264858210b78b540e57022b265a869921
ernestguevarra <ernestgmd@gmail.com>
2018-01-21 20:04:23 +0000

    update pull_remote.R

--- R/pull_remote.R
+++ R/pull_remote.R
@@ -1,10 +1,21 @@
 ################################################################################
 #
+#' pull_remote
 #'
+#' Pull ODK forms from remote ODK Aggregate via ODK Briefcase
 #'
-#'
-#'
-#'
+#' @params briefcase Path to ODK Briefcase jar file; default is jar file
+#' installed with package
+#' @params id Form ID of form to be pulled
+#' @params to Destination directory for pulled ODK forms
+#' @params remote URL of remote ODK Aggregate server
+#' @params username Username for account in remote ODK Aggregate server from
+#' which forms are to be pulled
+#' @params password Password for account in remote ODK Aggregate server from
+#' which forms are to be pulled
+#' @return Folder in destination directory named "ODK Briefcase Storage"
+#' containing forms pulled from remote ODK Aggregate server
+#' @examples
 #'
 pull_remote <- function(briefcase = "inst/java/ODK Briefcase v1.8.0 Production.jar",
                         id, to = getwd(), from, username, password) {
