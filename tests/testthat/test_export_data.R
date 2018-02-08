47d31f41df3f7a1ca3ff733e1c4999e762b36985
ernestguevarra <ernestgmd@gmail.com>
2018-02-08 06:21:13 +0000

    update export_data() test

--- tests/testthat/test_export_data.R
+++ tests/testthat/test_export_data.R
@@ -5,13 +5,16 @@ get_briefcase()
 
 pull_remote(id = "stakeholders",
             from = "https://ona.io/validtrial",
+            to = "~/Desktop",
             username = "validtrial",
             password = "zEF-STN-5ze-qom")
 
 export_data(id = "stakeholders",
             filename = "test.csv",
+            from = "~/Desktop",
+            to = "~/Desktop",
             overwrite = TRUE)
 
 test_that("Output CSV data exists", {
-  expect_true(file.exists(paste(getwd(), "/test.csv", sep = "")))
+  expect_true(file.exists("~/Desktop/test.csv", sep = ""))
 })
