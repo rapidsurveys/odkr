79236b98004efeeccf37641d059a9f26ca1950ca
ernestguevarra <ernestgmd@gmail.com>
2018-02-08 12:18:56 +0000

    update test for export_data()

--- tests/testthat/test_export_data.R
+++ tests/testthat/test_export_data.R
@@ -1,19 +1,17 @@
-library("odkr")
+#library("odkr")
 context("Check exported data")
 
+get_briefcase()
+
+pull_remote(id = "stakeholders",
+            from = "https://ona.io/validtrial",
+            username = "validtrial",
+            password = "zEF-STN-5ze-qom")
+
+export_data(id = "stakeholders",
+            filename = "test.csv",
+            overwrite = TRUE)
 
 test_that("Output CSV data exists", {
-
-  get_briefcase()
-
-  pull_remote(id = "stakeholders",
-              from = "https://ona.io/validtrial",
-              username = "validtrial",
-              password = "zEF-STN-5ze-qom")
-
-  export_data(id = "stakeholders",
-              filename = "test.csv",
-              overwrite = TRUE)
-
   expect_true(file.exists("test.csv", sep = ""))
 })
