2baea12469825c6245cb3d738c43515b568066d6
Ernest Guevarra <ernest@validmeasures.org>
2018-06-28 21:14:14 +0100

    add create_sd()

---
+++ R/create_sd.R
@@ -0,0 +1,23 @@
+################################################################################
+#
+#' create_sd
+#'
+#' Function to create an ODK Briefcase Storage directory
+#'
+#' @param path Directory path on which to create the ODK Briefcase Storage
+#'
+#' @return ODK Briefcase Storage directory in the specified path
+#'
+#' @examples
+#' create_sd(path = "Desktop")
+#'
+#' @export
+#'
+#'
+#
+################################################################################
+
+create_sd <- function(path) {
+  dir.create(path = paste(path, "/ODK Briefcase Storage", sep = ""))
+  dir.create(path = paste(path, "/ODK Briefcase Storage/forms", sep = ""))
+}
