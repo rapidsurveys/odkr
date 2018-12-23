6e4175372cfd7d32953788788efd33cb987a98ee
Ernest Guevarra <ernest@validmeasures.org>
2018-12-23 00:41:48 +0000

    create new package badge

---
+++ inst/createHexSticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker("inst/figures/odkHi.png",
+        s_x = 1, s_y = 0.8, s_width = 0.6,
+        package = "odkr", p_color = "#FFFFFF", p_y = 1.5, p_size = 12,
+        h_size = 1, h_color = "#996633", h_fill = "#D0AA82",
+        filename = "man/figures/odkr.png")
