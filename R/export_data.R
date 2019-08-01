f8738840d3feb46012e89405180ab8b138f5c61b
Ernest Guevarra <ernest@validmeasures.org>
2019-08-01 07:24:05 +0100

    correct export parameters code

--- R/export_data.R
+++ R/export_data.R
@@ -7,9 +7,9 @@
 #'
 #' @param target Path to directory of ODK Briefcase \code{.jar} file. Directory
 #'     path should match directory path used when calling \code{get_briefcase()}.
-#'     If ODK Briefcase \code{.jar} file was downloaded manually from \url{https://opendatakit.org},
-#'     \code{target} should match the directory path where \code{.jar} file has
-#'     been downloaded into.
+#'     If ODK Briefcase \code{.jar} file was downloaded manually from
+#'     \url{https://opendatakit.org}, \code{target} should match the directory
+#'     path where \code{.jar} file has been downloaded into.
 #' @param briefcase Filename of the downloaded ODK Briefcase \code{.jar} file.
 #'     Default is \code{odkBriefcase_latest} to match the default filename used
 #'     by \code{get_briefcase()}. If ODK Briefcase \code{.jar} file was
@@ -20,15 +20,15 @@
 #'     specified by \code{to}. Default is FALSE.
 #' @param id Form ID of form to be pulled
 #' @param from Path to source ODK Briefcase Storage from which to extract data.
-#'     This should match directory path specified when making a call to \code{pull_remote()}
-#'     or \code{pull_local()}
+#'     This should match directory path specified when making a call to
+#'     \code{pull_remote()} or \code{pull_local()}
 #' @param to Destination directory to save output data file
 #' @param filename Filename of output CSV data; default is
 #' \code{paste(id, "_data.csv", sep = "")}
 #' @param start Include data from submission dates after (inclusive) this start
-#' date in export to CSV; format <yyyy/MM/dd>
-#' @param end Include data from submission dates before (exclusive) this date in
-#' export to CSV; format <yyyy/MM/dd>
+#' date in export to CSV. Date format <yyyy-MM-dd> or <yyyy/MM/dd>
+#' @param end Include data from submission dates before (inclusive) this date in
+#' export to CSV. Date format <yyyy-MM-dd> or <yyyy/MM/dd>
 #' @param overwrite Overwrite existing output data in destination
 #' directory with the same filename; default is FALSE
 #' @param exclude Exclude media files on export; default is TRUE
@@ -112,7 +112,7 @@ export_data <- function(target = "", briefcase = "odkBriefcase_latest", sd = FAL
   # Add further specifications to command line inputs
   #
   if(!is.null(start)) z <- paste(z, " --export_start_date ", start, sep = "")
-  if(!is.null(end)) z <- paste(z, " --export_end_data ", end, sep = "")
+  if(!is.null(end)) z <- paste(z, " --export_end_date ", end, sep = "")
   if(overwrite == TRUE) z <- paste(z, " --overwrite_csv_export ", sep = "")
   if(exclude == TRUE) z <- paste(z, " --exclude_media_export ", sep = "")
   #
