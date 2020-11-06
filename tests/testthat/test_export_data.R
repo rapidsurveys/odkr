bf57f60779d7eacc7cebcf77382c61d73e3e9bdb
Ernest Guevarra <ernest@guevarra.io>
2020-11-06 08:53:50 +0000

    update tests

--- tests/testthat/test_export_data.R
+++ tests/testthat/test_export_data.R
@@ -4,28 +4,6 @@ context("Check exported data")
 library(testthat)
 library(odkr)
 
-dirPath <- tempdir()
-get_briefcase(destination = dirPath)
-
-pull_remote(target = dirPath,
-            id = "stakeholders",
-            from = "https://ona.io/validtrial",
-            to = dirPath,
-            username = "validtrial",
-            password = "zEF-STN-5ze-qom",
-            max_http_connections = 30)
-
-export_data(target = dirPath,
-            id = "stakeholders",
-            to = dirPath,
-            from = dirPath,
-            filename = "test.csv",
-            overwrite = TRUE)
-
-test_that("Output CSV data exists", {
-  expect_true(file.exists(paste(dirPath, "/test.csv", sep = "")))
-})
-
 test_that("Error for no target", {
   expect_error(export_data(id = "stakeholders",
                            to = dirPath,
